d6f20f69456bc9562918876eaa49739ff0929649
chandrashekarRravi <chandrashaker5412@gmail.com>
2025-12-27 20:57:40 +0530

    added sitemap for indexing

---
+++ app/sitemap.js
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    {
+        // after the deploy replace the original url 
+      url: "https://suyoginfra-ten.vercel.app/",
+      lastModified: new Date(),
+    },
+  ];
+}
